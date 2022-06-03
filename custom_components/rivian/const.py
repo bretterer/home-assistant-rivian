63bf9393a90b10be5c613969c0087814cda0fcbe
Brian Retterer <bretterer@gmail.com>
2022-06-02 22:01:48 -0400

    Add more sensors

--- custom_components/rivian/const.py
+++ custom_components/rivian/const.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 
 from typing import Final
 
-from homeassistant.const import LENGTH_MILES
+from homeassistant.const import LENGTH_MILES, LENGTH_KILOMETERS
 
 from .data_classes import RivianSensorEntity, RivianSensorEntityDescription
 
@@ -49,6 +49,22 @@ SENSORS: Final[dict[str, RivianSensorEntity]] = {
             icon="mdi:lock",
             key=f"{DOMAIN}_body_closure_global_closure_locked_state",
             native_unit_of_measurement=None,
+        )
+    ),
+    "energy_storage/vehicle_energy/vehicle_range": RivianSensorEntity(
+        entity_description=RivianSensorEntityDescription(
+            name="Estimated Vehicle Range",
+            icon="mdi:map-marker-distance",
+            key=f"{DOMAIN}_energy_storage_vehicle_energy_vehicle_range",
+            native_unit_of_measurement=LENGTH_MILES,
+        ),
+        value_lambda=lambda v: round(v * 0.6213711922, None),
+    ),
+    "energy_storage/charger/vehicle_charger_state": RivianSensorEntity(
+        entity_description=RivianSensorEntityDescription(
+            name="Charging Status",
+            icon="mdi:ev-station",
+            key=f"{DOMAIN}_energy_storage_charger_vehicle_charger_state",
         )
     ),
 }
