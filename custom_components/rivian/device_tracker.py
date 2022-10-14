a00f91589b61fd9c7fb6960be0da12ccfeb03c15
Brian Retterer <bretterer@gmail.com>
2022-10-13 22:01:45 -0400

    Fixes Location Position

--- custom_components/rivian/device_tracker.py
+++ custom_components/rivian/device_tracker.py
@@ -7,7 +7,10 @@ from homeassistant.components.device_tracker import SOURCE_TYPE_GPS
 from homeassistant.components.device_tracker.config_entry import TrackerEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant, callback
-from homeassistant.helpers.update_coordinator import CoordinatorEntity, DataUpdateCoordinator
+from homeassistant.helpers.update_coordinator import (
+    CoordinatorEntity,
+    DataUpdateCoordinator,
+)
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.entity import EntityDescription
 
@@ -27,7 +30,7 @@ async def async_setup_entry(
     coordinator = hass.data[DOMAIN][entry.entry_id][ATTR_COORDINATOR]
 
     entities = []
-    value = "$gnss"
+    value = "telematics/gnss/position"
     entities.append(RivianDeviceEntity(coordinator, entry, value))
 
     async_add_entities(entities, True)
@@ -47,9 +50,11 @@ class RivianDeviceEntity(CoordinatorEntity, TrackerEntity):
         self._attribute = attribute
         self._tracker_data = coordinator.data[self._attribute]
         self._config_entry = config_entry
-        self.entity_id = f"device_tracker.{DOMAIN}_gnss"
+        self.entity_id = f"device_tracker.{DOMAIN}_telematics_gnss_position"
         self._attr_name = "Rivian Location"
-        self.entity_description = EntityDescription(name="Rivian", key=f"{DOMAIN}_gnss")
+        self.entity_description = EntityDescription(
+            name="Rivian", key=f"{DOMAIN}_telematics_gnss_position"
+        )
 
     @property
     def unique_id(self) -> str:
@@ -64,32 +69,32 @@ class RivianDeviceEntity(CoordinatorEntity, TrackerEntity):
     @property
     def latitude(self) -> float | None:
         """Return latitude value of the device."""
-        return self._tracker_data[1]
+        return self._tracker_data[1]["lat"]
 
     @property
     def longitude(self) -> float | None:
         """Return longitude value of the device."""
-        return self._tracker_data[2]
+        return self._tracker_data[1]["lon"]
 
     @property
     def source_type(self):
         """Return the source type, eg gps or router, of the device."""
         return SOURCE_TYPE_GPS
 
-    @property
-    def location_accuracy(self) -> int:
-        return self._tracker_data[6]
+    # @property
+    # def location_accuracy(self) -> int:
+    #     return self._tracker_data[6]
 
-    @property
-    def extra_state_attributes(self):
-        """Return the state attributes of the device."""
-        return {
-            "trackr_id": self.unique_id,
-            "altitude": self._tracker_data[3],
-            "heading": self._tracker_data[4],
-            "speed": self._tracker_data[5],
-            "last_update": self._tracker_data[0],
-        }
+    # @property
+    # def extra_state_attributes(self):
+    #     """Return the state attributes of the device."""
+    #     return {
+    #         "trackr_id": self.unique_id,
+    #         "altitude": self._tracker_data[3],
+    #         "heading": self._tracker_data[4],
+    #         "speed": self._tracker_data[5],
+    #         "last_update": self._tracker_data[0],
+    #     }
 
     @callback
     def _handle_coordinator_update(self) -> None:
