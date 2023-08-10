720290e299d46dbbd946022eda042bc5f0dea655
Nathan Spencer <natekspencer@gmail.com>
2023-08-10 01:39:28 +0000

    Require 2FA for vehicle control

--- custom_components/rivian/helpers.py
+++ custom_components/rivian/helpers.py
@@ -31,3 +31,13 @@ TO_REDACT = {
 def redact(data: _T) -> dict:
     """Redact sensitive data."""
     return async_redact_data(data, TO_REDACT)
+
+
+def get_rivian_api_from_entry(entry: ConfigEntry) -> Rivian:
+    """Get Rivian API from a config entry."""
+    return Rivian(
+        request_timeout=30,
+        access_token=entry.data.get(CONF_ACCESS_TOKEN),
+        refresh_token=entry.data.get(CONF_REFRESH_TOKEN),
+        user_session_token=entry.data.get(CONF_USER_SESSION_TOKEN),
+    )
