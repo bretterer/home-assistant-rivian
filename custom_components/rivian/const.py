ef4d9666d55ddde3be3f2ab25561133adfa30161
Greg Rhoades <info@grhoades.net>
2026-08-02 17:47:22 -0400

    chore: update manifest and const version to 2.0.0-beta.1

--- custom_components/rivian/const.py
+++ custom_components/rivian/const.py
@@ -25,7 +25,7 @@ from .data_classes import (
 
 NAME = "Rivian (Unofficial)"
 DOMAIN = "rivian"
-VERSION = "0.0.0"
+VERSION = "2.0.0-beta.1"
 ISSUE_URL = "https://github.com/bretterer/home-assistant-rivian/issues"
 
 # Attributes
